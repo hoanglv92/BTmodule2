acd07dc1347accacb3a981894b6f138ee84d2ec8
hoanglv92 <hoanglv92@gmail.com>
2020-01-07 15:06:27 +0700

    btup

--- AbstractInterface/src/BtColorable/Main.java
+++ AbstractInterface/src/BtColorable/Main.java
@@ -7,12 +7,13 @@ public class Main {
         square[1]=new Square(5);
         square[2]=new Square();
         for (int i=0;i<square.length;i++){
-            System.out.println(square[i].getArea());
+            System.out.println("area "+"square"+i+ "="+square[i].getArea());
         }
         for(int j=0 ; j<square.length;j++){
             if (square[j].getColor()!=null){
                 System.out.println(square[j]);
             }else {
+                System.out.println("square "+j+"là :");
                square[j].howToColor();
             }
         }
