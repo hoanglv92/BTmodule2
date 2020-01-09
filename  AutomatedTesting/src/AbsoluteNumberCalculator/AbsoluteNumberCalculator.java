5a2b701bc1483265a9bbfdae40792da43afaf0ba
hoanglv92 <hoanglv92@gmail.com>
2020-01-09 11:11:31 +0700

    s

---
+++  AutomatedTesting/src/AbsoluteNumberCalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,7 @@
+package AbsoluteNumberCalculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsulute(int number){
+        throw new UnsupportedOperationException();
+    }
+}
---
+++  AutomatedTesting/test/AbsoluteNumberCalculator/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,10 @@
+package AbsoluteNumberCalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+@Test
+    void
+}
