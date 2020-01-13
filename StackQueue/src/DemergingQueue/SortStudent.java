6c41e3eb96eb9d4d3b9887943311a16a0b3b79b3
hoanglv92 <hoanglv92@gmail.com>
2020-01-13 22:28:55 +0700

    update

--- StackQueue/src/DemergingQueue/SortStudent.java
+++ StackQueue/src/DemergingQueue/SortStudent.java
@@ -12,8 +12,9 @@ public class SortStudent {
         sc.nextLine();
         System.out.println("nhập vào thông tin sinh viên.");
         LinkedList<Student>studentLinkedList=new LinkedList<>();
+        sortListByGender(studentLinkedList);
         for(int i=0;i<numberStudent;i++){
-            System.out.println("nhập tên:");
+            System.out.println("nhập tên Sv thứ :"+(i+1));
             String name=sc.nextLine();
             System.out.println("nhập id:");
             String id=sc.nextLine();
@@ -24,25 +25,45 @@ public class SortStudent {
             Student student=new  Student(name,id,gender,dateOfBirth);
             studentLinkedList.add(student);
         }
-
         Queue<Student>studentQueueMale=new LinkedList<Student>();
+        studentPushTostudentQueueMale(studentLinkedList,studentQueueMale);
         Queue<Student>studentQueueFemale=new LinkedList<Student>();
-
+        studentPushTostudentQueueFeMale(studentLinkedList,studentQueueFemale);
         LinkedList<Student>studentLinkedListNew=new LinkedList<Student>();
-        public void sortListByGender(LinkedList<Student>studentLinkedListNew) {
-            studentLinkedListNew.sort(new Comparator<Student>() {
-                @Override
-                public int compare(Student student1, Student student2) {
-                    return student1.getGender().compareTo(student2.getGender());
-                }
-            });
-            System.out.println("List is sorted");
+        pushToStudentLinkedListNew(studentQueueMale,studentQueueFemale,studentLinkedListNew);
+        for(Student st:studentLinkedListNew){
+            st.disPlay();
         }
-
-            for(Student st:studentLinkedList){
+    }
+    public  static void sortListByGender(LinkedList<Student>studentLinkedList){
+        Collections.sort(studentLinkedList,new Comparator<Student>() {
+            @Override
+            public int compare(Student student1, Student student2) {
+                return student1.getDateOfBirth().compareTo(student2.getDateOfBirth());
+            }
+        });
+        System.out.println("List is sorted");
+    }
+    public static void studentPushTostudentQueueMale(LinkedList<Student>studentLinkedList,Queue<Student>studentQueueMale){
+        for(Student st:studentLinkedList){
             if(st.getGender().equals("nam")){
                 studentQueueMale.add(st);
             }
+        }
+    }
+    public static void studentPushTostudentQueueFeMale(LinkedList<Student>studentLinkedList,Queue<Student>studentQueueFemale){
+        for(Student student:studentLinkedList){
+            if(student.getGender().equals("nu")){
+                studentQueueFemale.add(student);
+            }
+        }
+    }
+    public static void pushToStudentLinkedListNew(Queue<Student>studentQueueMale,Queue<Student>studentQueueFemale,LinkedList<Student>studentLinkedListNew){
+        while (!studentQueueFemale.isEmpty()){
+            studentLinkedListNew.add(studentQueueFemale.poll());
+        }
+        while (!studentQueueMale.isEmpty()){
+            studentLinkedListNew.add(studentQueueMale.poll());
         }
     }
 
